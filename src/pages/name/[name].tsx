9a7b132d899aee627979ae401768d168971c93c8
thedraxx <thedraxxgames@gmail.com>
2023-02-01 11:27:21 -0300

    change styles in home/name

--- src/pages/name/[name].tsx
+++ src/pages/name/[name].tsx
@@ -1,11 +1,17 @@
 /* eslint-disable react-hooks/rules-of-hooks */
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Layout from '@/components/layouts/Layout'
 import { GetStaticProps, GetStaticPaths } from 'next';
 import { pokeAPI } from '@/api';
 import { Pokemon } from '@/interfaces/pokemonFull';
-import { PokemonsListResponse } from '@/interfaces/pokemonsInterface';
-import Layout from '@/components/layouts/Layout';
+import { Grid, Card, Text, Button, Container, Image } from '@nextui-org/react';
+import { useRouter } from 'next/router';
+import { localFavorites } from '@/utilities';
+import ReactCanvasConfetti from 'react-canvas-confetti';
+import confetti from 'canvas-confetti';
 import getPokemonInfo from '@/utilities/getPokemonInfo';
+import { PokemonsListResponse } from '@/interfaces/pokemonsInterface';
+
 
 
 interface Props {
@@ -13,9 +19,60 @@ interface Props {
 }
 
 const index = ({ pokemon }: Props) => {
+
+    const [isInFavorite, setIsInFavorite] = useState(localFavorites.existsInFavorites(pokemon.id))
+
+    const onToggleFavorite = () => {
+        localFavorites.toggleFavorite(pokemon.id);
+        setIsInFavorite(!isInFavorite);
+
+        if (!isInFavorite) {
+            confetti({
+                particleCount: 100,
+                spread: 70,
+                origin: { y: 0.6, x: 0.7 }
+            });
+
+        }
+    }
+
     return (
         <Layout title={pokemon.name}>
-            {pokemon.name}
+            <Grid.Container css={{ marginTop: "5px" }} gap={2}>
+                <Grid xs={12} md={6}>
+                    <Card isHoverable css={{ padding: "100px" }}>
+                        <Card.Body>
+                            <Card.Image
+                                src={pokemon.sprites.other?.dream_world.front_default || "no image"}
+                                alt={pokemon.name}
+                                css={{ width: "100%", height: "auto", objectFit: "contain" }}
+                            />
+                        </Card.Body>
+                    </Card>
+                </Grid>
+
+                <Grid xs={12} md={6}>
+                    <Card isHoverable css={{ padding: "20px" }}>
+                        <Card.Body>
+                            <Text h1 transform='capitalize' >{pokemon.name}</Text>
+                            <p>Weight: {pokemon.weight}</p>
+                            <p>Height: {pokemon.height}</p>
+                            <p>Base experience: {pokemon.base_experience}</p>
+                            <p>Abilities: {pokemon.abilities.map((ability: any) => ability.ability.name).join(", ")}</p>
+                            <p>Types: {pokemon.types.map((type: any) => type.type.name).join(", ")}</p>
+                            <Card.Footer>
+                                <Container direction='row' display='flex'>
+                                    <Image src={pokemon.sprites.front_default} alt={pokemon.name} />
+                                    <Image src={pokemon.sprites.back_default} alt={pokemon.name} />
+                                    <Image src={pokemon.sprites.front_shiny} alt={pokemon.name} />
+                                    <Image src={pokemon.sprites.back_shiny} alt={pokemon.name} />
+                                </Container>
+                            </Card.Footer>
+                        </Card.Body>
+                        <Button color="gradient" ghost={!isInFavorite} onPress={onToggleFavorite}>Favorite</Button>
+                    </Card>
+                </Grid>
+            </Grid.Container>
         </Layout>
     )
 }
